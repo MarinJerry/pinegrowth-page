3901aa96d828fef201f93e390c9cc5680ab0881d
MarinJerry <software_development.marin@outlook.es>
2025-04-06 02:04:35 -0600

    DEPLOY

--- src/pages/index-three.js
+++ src/pages/index-three.js
@@ -11,7 +11,6 @@ import AgencyTab from "../components/agencyTab";
 import Cta from "../components/cta";
 import Client from "../components/client";
 import Pricing from "../components/pricing";
-import Blogs from "../components/blog";
 import GetInTouch from "../components/getInTuoch";
 import Footer from "../components/footer";
 import Switcher from "../components/switcher";
