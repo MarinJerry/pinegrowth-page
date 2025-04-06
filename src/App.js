d444575e1b8cd450cd24f14137d38b856f128d58
MarinJerry <software_development.marin@outlook.es>
2025-04-06 01:19:13 -0600

    Deploy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import './assets/css/tailwind.css'
 import './assets/css/materialdesignicons.min.css'
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Index from './pages';
 import IndexTwo from './pages/index-two';
 import IndexThree from './pages/index-three';
